3b1d4d15e440e7c70530466ac062881eb65ee5e9
RDV1KOR <RDV1KOR@BANI-C-00058.bmh.apac.bosch.com>
2021-04-14 00:39:19 +0530

    Added capabilities

--- vitals/Main.java
+++ vitals/Main.java
@@ -1,26 +1,15 @@
 package vitals;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
 public class Main {
-
-  @Test
-  public void testBatteryStatus()
-  {
-    assertTrue(LiionBatteryValidation.batteryIsOk(25, 70, 0.7f));
-    assertFalse(LiionBatteryValidation.batteryIsOk(50, 85, 0.0f));
-    assertFalse(LiionBatteryValidation.batteryIsOk(25, 15, 0.0f));
-    assertFalse(LiionBatteryValidation.batteryIsOk(25, 70, 0.9f));
-    assertFalse(LiionBatteryValidation.batteryIsOk(-10, 85, 0.0f));
-    assertFalse(LiionBatteryValidation.batteryIsOk(25, 10, 0.0f));
-    assertFalse(LiionBatteryValidation.batteryIsOk(25, 60, 1.0f));
-  }
 
   public static void main(final String[] args)
   {
-    //
+    assert LiionBatteryValidation.batteryIsOk(25, 70, 0.7f) == true;
+    assert LiionBatteryValidation.batteryIsOk(50, 85, 0.0f) == false;
+    assert LiionBatteryValidation.batteryIsOk(25, 15, 0.0f) == false;
+    assert LiionBatteryValidation.batteryIsOk(25, 70, 0.9f) == false;
+    assert LiionBatteryValidation.batteryIsOk(-10, 85, 0.0f) == false;
+    assert LiionBatteryValidation.batteryIsOk(25, 10, 0.0f) == false;
+    assert LiionBatteryValidation.batteryIsOk(25, 60, 1.0f) == false;
   }
 }
